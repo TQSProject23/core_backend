849b90120e0228da28ad9dd383f57a2e8bb866fa
r4sinhas <rasinhas@ua.pt>
2023-05-27 20:50:19 +0100

    testing updates - order controller

--- src/main/java/tqs/loadconnect/core_backend/models/Order.java
+++ src/main/java/tqs/loadconnect/core_backend/models/Order.java
@@ -61,4 +61,102 @@ public class Order {
     @JoinColumn(name = "store_id")
     @OnDelete(action = OnDeleteAction.CASCADE)
     private PartnerStore partnerStore;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public Date getDateOrdered() {
+        return dateOrdered;
+    }
+
+    public void setDateOrdered(Date dateOrdered) {
+        this.dateOrdered = dateOrdered;
+    }
+
+    public Date getExpectedDeliveryDate() {
+        return expectedDeliveryDate;
+    }
+
+    public void setExpectedDeliveryDate(Date expectedDeliveryDate) {
+        this.expectedDeliveryDate = expectedDeliveryDate;
+    }
+
+    public Date getPickup_date() {
+        return pickup_date;
+    }
+
+    public void setPickup_date(Date pickup_date) {
+        this.pickup_date = pickup_date;
+    }
+
+    public OrderStatusEnum getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatusEnum status) {
+        this.status = status;
+    }
+
+    public String getClientName() {
+        return clientName;
+    }
+
+    public void setClientName(String clientName) {
+        this.clientName = clientName;
+    }
+
+    public String getClientEmail() {
+        return clientEmail;
+    }
+
+    public void setClientEmail(String clientEmail) {
+        this.clientEmail = clientEmail;
+    }
+
+    public PickupPoint getPickupPoint() {
+        return pickupPoint;
+    }
+
+    public void setPickupPoint(PickupPoint pickupPoint) {
+        this.pickupPoint = pickupPoint;
+    }
+
+    public PartnerStore getPartnerStore() {
+        return partnerStore;
+    }
+
+    public void setPartnerStore(PartnerStore partnerStore) {
+        this.partnerStore = partnerStore;
+    }
+
+
 }
--- src/test/java/tqs/loadconnect/core_backend/controller/OrderControllerTest.java
+++ src/test/java/tqs/loadconnect/core_backend/controller/OrderControllerTest.java
@@ -9,12 +9,14 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import tqs.loadconnect.core_backend.Utils.Enums.OrderStatusEnum;
 import tqs.loadconnect.core_backend.controllers.OrderController;
 import tqs.loadconnect.core_backend.models.Order;
 import tqs.loadconnect.core_backend.models.PartnerStore;
 import tqs.loadconnect.core_backend.models.PickupPoint;
 import tqs.loadconnect.core_backend.services.OrderService;
 
+import java.util.Date;
 import java.util.List;
 
 
@@ -35,18 +37,85 @@ public class OrderControllerTest {
     private PickupPoint pickupPoint1;
     private PickupPoint pickupPoint2;
     private PickupPoint pickupPoint3;
+    private PickupPoint pickupPoint4;
 
-    private Order order1 = new Order("Description1", )
+    private Order order1;
     private Order order2;
     private Order order3;
 
     @BeforeEach
     void setUp() {
 
-        // create a couple of partner stores
-        partnerStore1 = new PartnerStore("Partner Store 1", "Rua do Partner Store 1", new List<Order>());
+        // ORDERS
+        order1 = new Order();
+        order1.setDescription("Item 1");
+        order1.setPrice(10.99f);
+        order1.setWeight(0.5f);
+        order1.setDateOrdered(new Date());
+        order1.setExpectedDeliveryDate(new Date());
+        order1.setPickup_date(new Date());
+        order1.setStatus(OrderStatusEnum.PENDING);
+        order1.setClientName("John Doe");
+        order1.setClientEmail("john@example.com");
+
+        order2 = new Order();
+        order2.setDescription("Item 2");
+        order2.setPrice(19.99f);
+        order2.setWeight(1.2f);
+        order2.setDateOrdered(new Date());
+        order2.setExpectedDeliveryDate(new Date());
+        order2.setPickup_date(new Date());
+        order2.setStatus(OrderStatusEnum.IN_TRANSIT);
+        order2.setClientName("Jane Smith");
+        order2.setClientEmail("jane@example.com");
+
+        order3 = new Order();
+        order3.setDescription("Item 3");
+        order3.setPrice(5.99f);
+        order3.setWeight(0.3f);
+        order3.setDateOrdered(new Date());
+        order3.setExpectedDeliveryDate(new Date());
+        order3.setPickup_date(new Date());
+        order3.setStatus(OrderStatusEnum.DELIVERED);
+        order3.setClientName("Alice Johnson");
+        order3.setClientEmail("alice@example.com");
+
+        // partner stores
+        partnerStore1 = new PartnerStore();
+        partnerStore1.setName("Store 1");
+        partnerStore1.setAddress("Address 1");
+
+        partnerStore2 = new PartnerStore();
+        partnerStore2.setName("Store 2");
+        partnerStore2.setAddress("Address 2");
 
 
+        // pickup points
+        pickupPoint1 = new PickupPoint();
+        pickupPoint1.setName("Pickup Point 1");
+        pickupPoint1.setAddress("Address 1");
+        pickupPoint1.setPartnerStore(partnerStore1);
+
+        pickupPoint2 = new PickupPoint();
+        pickupPoint2.setName("Pickup Point 2");
+        pickupPoint2.setAddress("Address 2");
+        pickupPoint2.setPartnerStore(partnerStore2);
+
+        pickupPoint3 = new PickupPoint();
+        pickupPoint3.setName("Pickup Point 3");
+        pickupPoint3.setAddress("Address 3");
+        pickupPoint3.setPartnerStore(partnerStore3);
+
+        pickupPoint4 = new PickupPoint();
+        pickupPoint4.setName("Pickup Point 4");
+        pickupPoint4.setAddress("Address 4");
+        pickupPoint4.setPartnerStore(partnerStore3);
+
+
+        // add orders to pickup points
+        pickupPoint1.setOrders(List.of(order1, order2));
+        pickupPoint2.setOrders(List.of(order3));
+        
     }
 
 }
