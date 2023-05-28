e6306bc59dc92d975860e937eb5d8a91813c78a0
r4sinhas <rasinhas@ua.pt>
2023-05-28 13:34:40 +0100

    small fix

--- src/main/java/tqs/loadconnect/core_backend/repositories/OrderRepository.java
+++ src/main/java/tqs/loadconnect/core_backend/repositories/OrderRepository.java
@@ -1,10 +1,12 @@
 package tqs.loadconnect.core_backend.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import tqs.loadconnect.core_backend.models.Order;
 
 import java.util.List;
 
+@Repository
 public interface OrderRepository extends JpaRepository<Order, Integer> {
 
     List<Order> findByPickupPointId(Integer pickupPointId);
