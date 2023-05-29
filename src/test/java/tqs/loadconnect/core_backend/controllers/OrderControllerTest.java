451bb8cfba7ca7ef4e93471b6ec4f260aa7df825
GoncaloSilva25 <goncassilva02@gmail.com>
2023-05-29 22:05:03 +0100

    add test files and change in build file

--- src/test/java/tqs/loadconnect/core_backend/controllers/OrderControllerTest.java
+++ src/test/java/tqs/loadconnect/core_backend/controllers/OrderControllerTest.java
@@ -1,22 +1,16 @@
 package tqs.loadconnect.core_backend.controllers;
 
-import jakarta.servlet.http.Part;
-import org.aspectj.weaver.ast.Or;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
 import tqs.loadconnect.core_backend.Utils.Enums.OrderStatusEnum;
-import tqs.loadconnect.core_backend.controllers.OrderController;
 import tqs.loadconnect.core_backend.models.Order;
 import tqs.loadconnect.core_backend.models.PartnerStore;
 import tqs.loadconnect.core_backend.models.PickupPoint;
